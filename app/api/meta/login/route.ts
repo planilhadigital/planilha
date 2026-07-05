313ffa1dfcca439d67cde2c90f3d9912f92f4d82
Jean Carlos Pereira Moreira <jean100pw@gmail.com>
2026-07-04 21:18:59 -0300

    fix: Removido escopo email para bypassar bloqueio da Meta

--- app/api/meta/login/route.ts
+++ app/api/meta/login/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const redirectUri = `${process.env.NEXTAUTH_URL}/api/meta/callback`
   
   // Permissões iniciais para conectar a conta (iremos pedir as do Instagram depois)
-  const scopes = 'public_profile,email,pages_show_list'
+  const scopes = 'public_profile,pages_show_list'
 
   const facebookAuthUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&response_type=code`
 
