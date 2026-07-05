d2e42badc00dbaf0ad04b984c05556239c0d0e4b
planilhadigital <pubagdstudio@gmail.com>
2026-07-05 20:15:00 -0300

    fix: add business_management scope to fetch all business pages

--- app/api/meta/login/route.ts
+++ app/api/meta/login/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
   const redirectUri = `${baseUrl}/api/meta/callback`
   
   // Permissões para App tipo Consumidor/Nenhum
-  const scopes = 'public_profile,pages_show_list,pages_read_engagement,pages_manage_posts,instagram_manage_insights,instagram_content_publish'
+  const scopes = 'public_profile,pages_show_list,pages_read_engagement,pages_manage_posts,instagram_manage_insights,instagram_content_publish,business_management'
 
   const facebookAuthUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&response_type=code`
 
